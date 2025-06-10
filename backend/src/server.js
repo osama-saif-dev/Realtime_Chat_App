7b5df992a56c26f42375cba1a1522280133583d7
osama <osamasaif242@gmail.com>
2025-06-10 15:20:09 +0300

    cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,10 +14,11 @@ const __dirName = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
+app.use(cors());
+// {
+//     origin: 'http://localhost:5173',
+//     credentials: true
+// }
 
 app.use('/api/auth', authRouter);
 app.use('/api/message', messageRouter);
